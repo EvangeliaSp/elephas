33a586d213b2e12f62c2466e7dafb13b2de8c16d
evangelia <evangelia@lana-labs.com>
2019-02-08 20:49:57 +0100

    Optimized imports

--- src/main/java/ElephasAPI.java
+++ src/main/java/ElephasAPI.java
@@ -1,6 +1,5 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
