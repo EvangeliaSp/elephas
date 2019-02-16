7448198630555654286129de70cbde166e03e4c8
mikaela.m <you@example.com>
2019-02-16 17:13:42 +0100

    Added product controller

---
+++ src/main/java/se/uu/elephas/controllers/ProductController.java
@@ -0,0 +1,32 @@
+package se.uu.elephas.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import se.uu.elephas.model.Product;
+import se.uu.elephas.services.ProductServiceimpl;
+
+
+
+@RestController
+@RequestMapping
+@CrossOrigin("*")
+public class ProductController {
+
+    @Autowired
+    private ProductServiceimpl productService;
+
+    @RequestMapping(value = "/create", method = {RequestMethod.POST})
+    public ResponseEntity<String> create(@RequestBody Product product) throws Exception {
+
+
+            productService.create(product);
+
+
+        return ResponseEntity.status(HttpStatus.OK).body(new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(product));
+
+    }
+
+}
