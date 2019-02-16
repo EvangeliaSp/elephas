c1265d26cb43f1c0dbdd61b2a2f79a19b916469a
evangelia <evangelia@lana-labs.com>
2019-02-16 14:09:59 +0100

    DB Model for the order

--- src/main/java/se/uu/elephas/model/Order.java
+++ src/main/java/se/uu/elephas/model/Order.java
@@ -1,39 +1,46 @@
 package se.uu.elephas.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
+import java.sql.Timestamp;
 
 @Entity
 @Table(name = "order")
 public class Order {
-  
+
     @Id
     @GeneratedValue
-    private Long idOrder;
+    private Long id;
 
-    //@Id
+    @Column(nullable = false)
     private Long idUser;
 
-    private Status status; //FIXME: how to represent enum?
-    private int paymentStatus; //FIXME: how to represent tinyint?
-    // private ??? date; //TODO: what type is good to use for this?
+    private Boolean confirm; //FIXME: how to represent tinyint?
+
     private float sum;
-    private int confirm; //FIXME: how to represent tinyint?
+
+    private Timestamp date;
+
+    private int status; //FIXME: how to represent enum?
+
+    private Boolean paymentStatus;
+
+    private int paymentType;
 
     public Order() {}
 
-    public Order(Long idUser) {
+    // Constructor of basket
+    public Order(Long idUser, Boolean confirm, float sum) {
         this.idUser = idUser;
+        this.confirm = confirm;
+        this.sum = sum;
     }
 
     public Long getIdOrder() {
-        return this.idOrder;
+        return this.id;
     }
 
     public void setIdOrder(Long id) {
-        this.idOrder = id;
+        this.id = id;
     }
 
     public Long getIdUser() {
@@ -44,32 +51,14 @@ public class Order {
         this.idUser = id;
     }
 
-    public Status getStatus() {
-        return this.status;
+    public Boolean getConfirm() {
+        return this.confirm;
     }
 
-    public void setStatus(Status s) {
-        this.status = s;
+    public void setConfirm(Boolean confirm) {
+        this.confirm = confirm;
     }
 
-    public int getPaymentStatus() {
-        return this.paymentStatus;
-    }
-
-    public void setPaymentStatus(int paymentStatus) {
-        this.paymentStatus = paymentStatus;
-    }
-
-    /* 
-    public ??? getDate() {
-        return this.date;
-    }
-
-    public void setDate(??? date) {
-        this.date = date;
-    }
-    */
-    
     public float getSum() {
         return this.sum;
     }
@@ -78,12 +67,55 @@ public class Order {
         this.sum = sum;
     }
 
-    public int getConfirm() {
-        return this.confirm;
+    public Timestamp getDate() {
+        return date;
     }
 
-    public void setConfirm(int confirm) {
-        this.confirm = confirm;
+    public void setDate(Timestamp date) {
+        this.date = date;
     }
 
+    public Status getStatus() {
+        switch (this.status) {
+            case 1:
+                return Status.COMPLETED;
+            case 2:
+                return Status.CANCELLED;
+            case 3:
+                return Status.SHIPPED;
+            case 4:
+                return Status.IN_PROGRESS;
+            default:
+                return null;
+        }
+    }
+
+    public void setStatus(Status s) {
+        this.status = s.getValue();
+    }
+
+    public Boolean getPaymentStatus() {
+        return this.paymentStatus;
+    }
+
+    public void setPaymentStatus(Boolean paymentStatus) {
+        this.paymentStatus = paymentStatus;
+    }
+
+    public PaymentType getPaymentType() {
+        switch (this.paymentType) {
+            case 1:
+                return PaymentType.CREDIT_CARD;
+            case 2:
+                return PaymentType.PAYPAL;
+            case 3:
+                return PaymentType.SWISH;
+            default:
+                return null;
+        }
+    }
+
+    public void setPaymentType(PaymentType paymentType) {
+        this.paymentType = paymentType.getValue();
+    }
 }
