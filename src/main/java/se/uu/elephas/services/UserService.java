b2c696a684f956e59c06a0136f11c442789ce279
evangelia <evangelia@lana-labs.com>
2019-02-16 17:07:07 +0100

    Optimized imports

--- src/main/java/se/uu/elephas/services/UserService.java
+++ src/main/java/se/uu/elephas/services/UserService.java
@@ -3,7 +3,6 @@ package se.uu.elephas.services;
 import se.uu.elephas.model.User;
 
 import java.security.MessageDigest;
-import java.util.List;
 import java.util.Optional;
 
 public interface UserService {
