0ce069d48b643cb3156610c37eb983a440f02ee3
mikaela.m <you@example.com>
2019-02-16 16:27:35 +0100

    Added Product repository

---
+++ src/main/java/se/uu/elephas/repository/ProductRepository.java
@@ -0,0 +1,32 @@
+package se.uu.elephas.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import se.uu.elephas.model.Product;
+
+import java.util.Optional;
+
+
+@Repository
+//@Transactional
+public interface ProductRepository extends CrudRepository<Product, Long> {
+
+    Page<Product> findAll(Pageable pageable);
+
+    Optional<Product> findByMaterial(int material);
+
+    Optional<Product> findByType(int type);
+
+    Optional<Product> findByColor(int color);
+
+    Optional<Product> findByMaterialandType(int material, int type);
+
+    Optional<Product> findByMaterialandColor(int material, int color);
+
+    Optional<Product> findByColorandType(int color, int type);
+
+    Optional<Product> findByMaterialandTypeandColor(int material, int type, int color);
+}
