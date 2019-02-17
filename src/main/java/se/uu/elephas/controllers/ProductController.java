5160b4ddabdc9d81b2e3da6c0c0a72d44ec6cc8e
mikaela.m <you@example.com>
2019-02-17 17:49:05 +0100

    Added findById

--- src/main/java/se/uu/elephas/controllers/ProductController.java
+++ src/main/java/se/uu/elephas/controllers/ProductController.java
@@ -1,5 +1,6 @@
 package se.uu.elephas.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.*;
 import se.uu.elephas.model.Product;
 import se.uu.elephas.services.ProductServiceimpl;
 
+import javax.validation.Valid;
+import java.util.Optional;
 
 
 @RestController
@@ -26,5 +29,20 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.OK).body(new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(product));
 
     }
+
+    @RequestMapping(value = "/findById", method = {RequestMethod.GET})
+    public ResponseEntity<String> findById(
+            @RequestParam("idProduct") @Valid Long idProduct)
+            throws JsonProcessingException {
+
+        Optional<Product> product = productService.getById(idProduct);
+
+        return product.isPresent()
+                ? ResponseEntity.status(HttpStatus.OK).body(new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(product.get()))
+                : ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product with id " + idProduct + " does not exist.");
+
+    }
+
+
 
 }
