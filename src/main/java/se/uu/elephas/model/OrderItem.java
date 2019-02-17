784913d79a92c62758e58e102a5cdfbe1fc2ff4d
evangelia <evangelia@lana-labs.com>
2019-02-17 20:11:39 +0100

    incomplete order item model

---
+++ src/main/java/se/uu/elephas/model/OrderItem.java
@@ -0,0 +1,43 @@
+package se.uu.elephas.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "order_item_table")
+public class OrderItem {
+
+    @Id
+    @GeneratedValue
+    private Long idOrderItem;
+
+    private int quantity;
+
+
+//    @ManyToOne
+//    @JoinColumn(name = "order_id", nullable = false)
+//    private Order
+
+    //@OneToOne
+//    @JoinColumn(name = "product_id")
+//    @RestResource(path = "libraryAddress", rel="address")
+    //private Product product;
+
+
+    public OrderItem() {}
+
+    public Long getIdOrderItem() {
+        return idOrderItem;
+    }
+
+    public void setIdOrderItem(Long idOrderItem) {
+        this.idOrderItem = idOrderItem;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
