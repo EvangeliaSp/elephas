8222edf83acb10867200f53dee939a9cf6088561
nicolets <Nikoletta.Tsongari.9603@student.uu.se>
2019-02-17 22:52:02 +0100

    elephas header attempth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,45 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
+import logo from './elephas.jpg';
 import './App.css';
 
-class App extends Component {
-
-  state = {};
-
-  componentDidMount() {
-    setInterval(this.hello, 250);
-  }
-
-  hello = () => {
-    fetch('/user/')
-        .then(response => response.text())
-        .then(message => {
-          this.setState({message: message});
-        });
-  };
-
-  render() {
-    return (
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo"/>
-            <h1 className="App-title">{this.state.message}</h1>
-          </header>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
-        </div>
-    );
-  }
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Header/>
+                <Content/>
+            </div>
+        );
+    }
 }
+class Header extends React.Component {
+    render() {
+        return (
+            <div className="header">
+                <a href="#default" className="logo">
+                    <img src={logo} alt="logo" width="128" height="64" margin-top="50px"/> </a>
+                <div className="header-right">
+                    <a className="active" href="#home">Home</a>
+                    <a  href="#bracelets">Bracelets</a>
+                    <a href="#rings">Rings</a>
+                    <a  href="#necklaces">Necklaces</a>
+                    <a  href="#earrings">Earrings</a>
 
+                    <a href="#contact">Contact</a>
+                    <a href="#about">About</a>
+                </div>
+            </div>
+        );
+    }
+}
+class Content extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Content</h2>
+                <p>The content text!!!</p>
+            </div>
+        );
+    }
+}
 export default App;
