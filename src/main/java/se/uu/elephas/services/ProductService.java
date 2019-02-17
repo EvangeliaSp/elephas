bc148c1e30b696df3761eae410b354a22019d33c
mikaela.m <you@example.com>
2019-02-17 17:50:18 +0100

    same

--- src/main/java/se/uu/elephas/services/ProductService.java
+++ src/main/java/se/uu/elephas/services/ProductService.java
@@ -2,8 +2,12 @@ package se.uu.elephas.services;
 
 import se.uu.elephas.model.Product;
 
+import java.util.Optional;
+
 public interface ProductService {
 
     Object create(Product product);
+
+    Optional<Product> getById(Long idProduct);
 
 }
