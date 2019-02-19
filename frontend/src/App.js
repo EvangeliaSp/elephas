c7e8200f3280745dfafc0712556b58c52f6520fa
nicolets <Nikoletta.Tsongari.9603@student.uu.se>
2019-02-19 14:46:04 +0100

    header changes as proposedin mid term presentation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import logo from './elephas.jpg';
+import logo from './elephas2.jpg';
 import './App.css';
 
 class App extends React.Component {
@@ -12,26 +12,40 @@ class App extends React.Component {
         );
     }
 }
+
 class Header extends React.Component {
     render() {
         return (
             <div className="header">
-                <a href="#default" className="logo">
-                    <img src={logo} alt="logo" width="128" height="64" margin-top="50px"/> </a>
+
                 <div className="header-right">
                     <a className="active" href="#home">Home</a>
-                    <a  href="#bracelets">Bracelets</a>
+                    <a href="#bracelets">Bracelets</a>
                     <a href="#rings">Rings</a>
-                    <a  href="#necklaces">Necklaces</a>
-                    <a  href="#earrings">Earrings</a>
+                    <a href="#necklaces">Necklaces</a>
+                    <a href="#earrings">Earrings</a>
 
                     <a href="#contact">Contact</a>
                     <a href="#about">About</a>
+
+                    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css"
+                          integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ"
+                          crossOrigin="anonymous"/>
+                    <div className="user">
+                        <a href="#cart"> <i class="fas fa-shopping-cart" rel="stylesheet"></i> </a>
+                    </div>
+                    <div className="cart">
+                        <a href="#user"> <i className="fas fa-user"></i> </a>
+
+                    </div>
                 </div>
             </div>
+
+
         );
     }
 }
+
 class Content extends React.Component {
     render() {
         return (
@@ -42,4 +56,5 @@ class Content extends React.Component {
         );
     }
 }
+
 export default App;
