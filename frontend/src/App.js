86590100a382b60c7f8d632d3823eea484b147c2
nicolets <Nikoletta.Tsongari.9603@student.uu.se>
2019-02-22 13:15:05 +0100

    footer updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,37 +19,36 @@ class Header extends React.Component {
         return (
             <div>
 
-            <div className="header">
-                <a href="#default" className="logo"> <img src={logo} alt="Logo"
-                                                          style={{width: "46px", height: "60px"}}/>
-                </a>
+                <div className="header">
+                    <a href="#default" className="logo"> <img src={logo} alt="Logo"
+                                                              style={{width: "46px", height: "60px"}}/>
+                    </a>
 
-                <div className="header-right">
+                    <div className="header-right">
 
 
-                    <a className="active" href="#home">Home</a>
-                    <a href="#bracelets">Bracelets</a>
-                    <a href="#rings">Rings</a>
-                    <a href="#necklaces">Necklaces</a>
-                    <a href="#earrings">Earrings</a>
+                        <a className="active" href="#home">Home</a>
+                        <a href="#bracelets">Bracelets</a>
+                        <a href="#rings">Rings</a>
+                        <a href="#necklaces">Necklaces</a>
+                        <a href="#earrings">Earrings</a>
 
-                    <a href="#contact">Contact</a>
-                    <a href="#about">About</a>
+                        <a href="#contact">Contact</a>
+                        <a href="#about">About</a>
 
-                    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css"
-                          integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ"
-                          crossOrigin="anonymous"/>
-                    <div className="user">
-                        <a href="#cart"> <i class="fas fa-shopping-cart" rel="stylesheet"></i> </a>
-                    </div>
-                    <div className="cart">
-                        <a href="#user"> <i className="fas fa-user"></i> </a>
+                        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css"
+                              integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ"
+                              crossOrigin="anonymous"/>
+                        <div className="user">
+                            <a href="#cart"> <i class="fas fa-shopping-cart" rel="stylesheet"></i> </a>
+                        </div>
+                        <div className="cart">
+                            <a href="#user"> <i className="fas fa-user"></i> </a>
 
+                        </div>
                     </div>
                 </div>
             </div>
-            </div>
-
 
 
         );
@@ -61,8 +60,21 @@ class Content extends React.Component {
     render() {
         return (
             <div>
-                <h2>Content</h2>
-                <p>The content text!!!</p>
+                <div className="fb">
+                    <iframe
+                        src="http://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Felephasjewelry&width=600&colorscheme=light&show_faces=true&border_color&stream=true&header=true&height=435"
+                        scrolling="yes"
+                        style={{
+                            border: "none",
+                            overflow: "hidden",
+                            width: "500px",
+                            height: "400px",
+                            background: "white",
+                        }}
+
+                        allowTransparency="true"></iframe>
+
+                </div>
             </div>
         );
     }
@@ -72,7 +84,15 @@ class Footer extends Component {
     render() {
         return (
             <div className="footer">
-                <i className="far fa-copyright"> Copyright 2019</i>
+                <div>
+                    <img src={logo} alt="Logo"
+                         style={{width: "10px", height: "12px"}}/>
+                    &nbsp;
+
+
+                    Konstantina Manousaki
+                </div>
+
                 <div><a href="https://www.instagram.com/elephas_heart_made_jewelry/" target="_blank"> <i
                     className="fab fa-instagram" style={{color: "#92af75"}}></i></a>
                     &nbsp;
@@ -81,10 +101,11 @@ class Footer extends Component {
                                                                                           style={{color: "#92af75"}}></i>
                     </a>
                 </div>
-
-                <div>Elephas - Konstantina Manousaki</div>
+                <i className="far fa-copyright"> Copyright 2019</i>
 
             </div>
+
+
         )
     }
 }
