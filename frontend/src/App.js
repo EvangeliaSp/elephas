d8c9f173c3cd3e635dbe38f9da275371e67a892a
nicolets <Nikoletta.Tsongari.9603@student.uu.se>
2019-02-26 13:42:13 +0100

    photo added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import logo from './logo2.png';
 import './App.css';
+import product1 from './product2.jpg'
 
 class App extends React.Component {
     render() {
@@ -60,6 +61,9 @@ class Content extends React.Component {
     render() {
         return (
             <div>
+                <div className="elephants">
+                    <img src={product1} alt="Elephas"/>
+                </div>
                 <div className="fb">
                     <iframe
                         src="http://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Felephasjewelry&width=600&colorscheme=light&show_faces=true&border_color&stream=true&header=true&height=435"
