299bab52069d87c1cb5ed2e634109cce685d7a85
knikamo <karolina@nikamo.se>
2019-02-27 22:42:48 +0100

    orders are shown under the tab in profile page

--- frontend/src/user/profile/Profile.js
+++ frontend/src/user/profile/Profile.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import Avatar from 'react-avatar';
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
+import { HashLink as Link } from 'react-router-hash-link';
 import './Profile.css'
 
 // import UserProfile from 'react-user-profile'
@@ -12,6 +13,7 @@ class Profile extends Component {
         super(props, context);
         this.state = {
             user: '',
+            orders: [],
             key: 'profile',
             path: ''
         };
@@ -19,6 +21,14 @@ class Profile extends Component {
 
     componentDidMount() {
         this.loadUserFromServer()
+        this.loadOrdersFromServer()
+    }
+
+    loadOrdersFromServer = () => {
+        const { id } = this.props.match.params
+        fetch(`/order/userOrders?idUser=${id}`) 
+            .then(response => response.json())
+            .then(data => this.setState({orders: data}))
     }
 
     loadUserFromServer = () => {
@@ -29,117 +39,159 @@ class Profile extends Component {
             .then(data => this.setState({user: data, path: pathLink}))
     };
 
-    render() {
-        const  {user, key, path} = this.state
-
+    setClassName = (str, oldName) => {
+        if (str === window.location.hash)
+            return oldName + "active";
+        return oldName;
+    }
+    tabMenu() {
         return (
-            <div>
-            <Header/>
-            <div id="user-profile-2" className="user-profile">
-                <div className="tabbable">
-                    <ul className="nav nav-tabs padding-18">
-                        <li className="active">
-                            <a data-toggle="tab" href="#home">
-                                <i className="green ace-icon fa fa-user bigger-120"></i>
-                                Profile
-                            </a>
-                        </li>
+            <ul className="nav nav-tabs padding-18">
+                <li className={this.setClassName("#profile", "")}>
+                    <Link data-toggle="tab" to="#profile">
+                        <i className="green ace-icon fa fa-user bigger-120"></i>
+                        Profile
+                    </Link>
+                </li>
 
-                        <li>
-                            <a data-toggle="tab" href="#pictures">
-                                <i className="pink ace-icon fa fa-picture-o bigger-120"></i>
-                                Orders
-                            </a>
-                        </li>
-                    </ul>
+                <li className={this.setClassName("#orders", "")}>
+                    <Link data-toggle="tab" to="#orders">
+                        <i className="pink ace-icon fa fa-picture-o bigger-120"></i>
+                        Orders
+                    </Link>
+                </li>
+            </ul>
+        );
+    }
 
-                    <div className="tab-content no-border padding-24">
-                        <div id="home" className="tab-pane in active">
-                            <div className="row">
-                                <div className="col-xs-12 col-sm-3 center">
+
+    profileInfo(user) {
+        return (
+            <div id="profile" className={this.setClassName("#profile", "tab-pane")}>
+                <div className="row">
+                    <div className="col-xs-12 col-sm-3 center">
                         <span className="profile-picture">
                             <Avatar className="editable img-responsive" alt=" Avatar" id="avatar2" name={user.firstname+' '+user.lastname} size="150"/>
                         </span>
 
-                                    <div className="space space-4"></div>
+                        <div className="space space-4"></div>
+                    </div>
 
-                                </div>
+                    <div className="col-xs-12 col-sm-9">
+                        <h4 className="blue">
+                            <span className="middle">{user.firstname+' '+user.lastname} </span>
 
-                                <div className="col-xs-12 col-sm-9">
-                                    <h4 className="blue">
-                                        <span className="middle">{user.firstname+' '+user.lastname} </span>
+                        </h4>
 
-                                    </h4>
+                        <div className="profile-user-info">
+                            <div className="profile-info-row">
+                                <div className="profile-info-name"> Email</div>
 
-                                    <div className="profile-user-info">
-                                        <div className="profile-info-row">
-                                            <div className="profile-info-name"> Email</div>
-
-                                            <div className="profile-info-value">
-                                                <span>{user.email}</span>
-                                            </div>
-                                        </div>
-
-                                        <div className="profile-info-row">
-                                            <div className="profile-info-name"> Location</div>
-
-                                            <div className="profile-info-value">
-                                                {/*<i className="fa fa-map-marker light-orange bigger-110"></i>*/}
-                                                <span>{user.city}</span>
-                                                <span>{user.country}</span>
-                                            </div>
-                                        </div>
-
-                                        <div className="profile-info-row">
-                                            <div className="profile-info-name"> Address</div>
-
-                                            <div className="profile-info-value">
-                                                {/*<i className="fa fa-map-marker light-orange bigger-110"></i>*/}
-                                                <span>{user.streetName}</span>
-                                                <span>{user.streetNumber}</span>
-                                                <span>{user.zipCode}</span>
-                                            </div>
-                                        </div>
-
-                                        <div className="profile-info-row">
-                                            <div className="profile-info-name"> Telephone</div>
-
-                                            <div className="profile-info-value">
-                                                <span>{user.telephone}</span>
-                                            </div>
-                                        </div>
-                                    </div>
-
+                                <div className="profile-info-value">
+                                    <span>{user.email}</span>
                                 </div>
                             </div>
 
-                            <div className="space-20"></div>
+                            <div className="profile-info-row">
+                                <div className="profile-info-name"> Location</div>
 
-                            <div className="row">
-                                <div className="col-xs-12 col-sm-6">
-                                    <div className="widget-box transparent">
-                                    </div>
+                                <div className="profile-info-value">
+                                    {/*<i className="fa fa-map-marker light-orange bigger-110"></i>*/}
+                                    <span>{user.city}</span>
+                                    <span>{user.country}</span>
+                                </div>
+                            </div>
+
+                            <div className="profile-info-row">
+                                <div className="profile-info-name"> Address</div>
+
+                                <div className="profile-info-value">
+                                    {/*<i className="fa fa-map-marker light-orange bigger-110"></i>*/}
+                                    <span>{user.streetName}</span>
+                                    <span>{user.streetNumber}</span>
+                                    <span>{user.zipCode}</span>
+                                </div>
+                            </div>
+
+                            <div className="profile-info-row">
+                                <div className="profile-info-name"> Telephone</div>
+
+                                <div className="profile-info-value">
+                                    <span>{user.telephone}</span>
                                 </div>
                             </div>
                         </div>
 
-                        <div id="pictures" className="tab-pane">
-                            <ul className="ace-thumbnails">
-                                <li>
-                                    <a href="#" data-rel="colorbox">
-                                        <img alt="150x150" src="http://lorempixel.com/200/200/nature/1/"/>
-                                        <div className="text">
-                                            <div className="inner">Sample Caption on Hover</div>
-                                        </div>
-                                    </a>
-                                </li>
+                    </div>
+                </div>
 
-                            </ul>
+                <div className="space-20"></div>
+
+                <div className="row">
+                    <div className="col-xs-12 col-sm-6">
+                        <div className="widget-box transparent">
                         </div>
                     </div>
                 </div>
             </div>
 
+        );
+    }
+
+   
+    usersOrderList(orders) {
+        return (
+            <div id="orders" className={this.setClassName("#orders", "tab-pane")}>
+                <table class="table table-striped ">
+                    <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Confirm</th>
+                        <th scope="col">Date</th>
+                        <th scope="col">Payment Status</th>
+                        <th scope="col">Payment Type</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">Sum</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {orders.map(order => (
+                    <tr key={order.idOrder}>
+                        <th scope="row">{order.idOrder}</th>
+                        <td>{order.confirm}</td>
+                        <td>{order.date}</td>
+                        <td>{order.paymentStatus}</td>
+                        <td>{order.paymentType}</td>
+                        <td>{order.status}</td>
+                        <td>{order.sum}</td>
+                    </tr>
+                ))}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+
+    
+
+    render() {
+        const  {user, orders, key, path} = this.state
+
+        return (
+            <div>
+            <Header/>
+
+            <div id="user-profile-2" className="user-profile">
+                <div className="tabbable">
+                    {this.tabMenu()}
+
+                    <div className="tab-content no-border padding-24" style={{marginBottom: "7rem"}}>
+                        {this.profileInfo(user)}
+                        {this.usersOrderList(orders)}
+                    </div>
+
+                </div>
+            </div>
 
             <Footer/>
             </div>
@@ -148,4 +200,4 @@ class Profile extends Component {
     }
 }
 
-export default Profile
+export default Profile;
