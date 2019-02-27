18bffef7dd73658755053dc539b727ebd32a5a0a
nicolets <Nikoletta.Tsongari.9603@student.uu.se>
2019-02-27 21:58:21 +0100

    header and footer tests

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo2.png';
 import './App.css';
 import product1 from './product2.jpg'
 
+
 class App extends React.Component {
     render() {
         return (
@@ -14,6 +15,8 @@ class App extends React.Component {
         );
     }
 }
+
+
 
 class Header extends React.Component {
     render() {
@@ -64,6 +67,8 @@ class Content extends React.Component {
                 <div className="elephants">
                     <img src={product1} alt="Elephas"/>
                 </div>
+                <div className="intro">Welcome to Elephas.. HeartMade Jewelry since 2016</div>
+                <div className="bestSeller">Most Recent Products</div>
                 <div className="fb">
                     {/*<iframe*/}
                     {/*src="http://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Felephasjewelry&width=600&colorscheme=light&show_faces=true&border_color&stream=true&header=true&height=435"*/}
