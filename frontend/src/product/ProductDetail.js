0fca56367de3b6eda651958d692dbcf0a7763c76
knikamo <karolina@nikamo.se>
2019-02-28 16:32:27 +0100

    component for displaying details of a single products (room for design improvements)

---
+++ frontend/src/product/ProductDetail.js
@@ -0,0 +1,80 @@
+import React, {Component} from 'react';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
+import elephas2 from './../elephas2.jpg';
+import Header from './../components/Header';
+import Footer from './../components/Footer';
+import {getColor, getMaterial, getType} from './Translations';
+
+class ProductDetail extends Component {
+
+    state = {};
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            product: '',
+            isLoading: true
+        }
+    }
+
+    componentDidMount() {
+        this.loadProductFromServer()
+    }
+
+    loadProductFromServer = () => {
+        this.setState({isLoading: true});
+        const { id } = this.props.match.params;
+        fetch(`/product/findById?idProduct=${id}`)
+            .then(response => response.json())
+            .then(data => this.setState({product: data, isLoading: false}))
+    };
+
+
+    ProductDetailCard(product) {
+        return(
+            <div key={product.idProduct} className="col-sm-12 col-lg-8 mx-auto">
+            <Card style={{ marginBottom: '2rem'}}>
+                <Card.Img variant="top" src={elephas2} />
+                <Card.Body>
+                    <Card.Title>{product.name}</Card.Title> 
+                    <Card.Text>
+                        {product.description}<br/>
+                        Made of: {getMaterial(product.material)}
+                    </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                {product.price} kr
+                <Button variant="primary" href="#addToBasket" style={{float:'right'}}>Add to basket</Button>
+                </Card.Footer>
+            </Card>
+            </div>
+        );
+    }
+
+ 
+
+    render() {
+        const  {product, isLoading} = this.state
+
+        if (isLoading)
+            return <p>Loading...</p>
+
+        return (
+            <div>
+                <Header/>
+                <Container>
+                    <Row style={{marginBottom:'7rem'}}>
+                        {this.ProductDetailCard(product)}
+                    </Row>
+                </Container>
+                <Footer/>
+            </div>
+        );
+    }
+}
+
+export default ProductDetail;
