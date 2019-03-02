78b46c2c4bec7ec2a5b06d41f6e9f3a5ac76d7bc
evangelia <evangelia@lana-labs.com>
2019-03-02 15:26:42 +0100

    Added increase and decrease item from basket

--- frontend/src/order/ShowCart.js
+++ frontend/src/order/ShowCart.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
+import {Redirect} from 'react-router-dom';
+import {ButtonToolbar, ListGroupItem} from "react-bootstrap";
 import Header from "../components/Header";
-import {MDBBtn} from "mdbreact";
+import {Container, MDBBtn} from "mdbreact";
 import './ShowCart.css'
 
 class ShowCart extends Component {
@@ -35,10 +37,32 @@ class ShowCart extends Component {
 
         fetch(`/order/removeFromBasket?idUser=${idUser}&idOrderItem=${idItem}`, options)
             .then((response) => {
-            return response.json();
-        }).then((result) => {
-            // do what you want with the response here
-        });
+                return <Redirect to={ ShowCart }/>
+            });
+
+    }
+
+    handleIncrease = (idUser, idItem) => {
+        const options = {
+            method: 'PATCH'
+        };
+
+        fetch(`/order/increase?idUser=${idUser}&idItem=${idItem}`, options)
+            .then((response) => {
+                return <Redirect to={ ShowCart }/>
+            });
+
+    }
+
+    handleDecrease = (idUser, idItem) => {
+        const options = {
+            method: 'PATCH'
+        };
+
+        fetch(`/order/decrease?idUser=${idUser}&idItem=${idItem}`, options)
+            .then((response) => {
+                return <Redirect to={ ShowCart }/>
+            });
 
     }
 
@@ -51,24 +75,34 @@ class ShowCart extends Component {
         return (
             <div>
                 <Header/>
+                <br/>
+                <Container>
                 <div>
                     <table className="table table-striped ">
+                        <tHeader><h3 align="center"><b>Shopping cart</b></h3></tHeader>
 
                         <tbody>
+                        <br/>
                         {items.map(item => (
                             <tr key={item.id}>
-                                <td>{item.id}</td>
                                 <td><img src={"https://i.pinimg.com/originals/96/f5/91/96f5916ce8fcc48004451e9a4895fd68.jpg"} width="200" height="100"/>  </td>
                                 <td>{item.name}</td>
                                 <td>{item.price} kr</td>
-                                <td>{item.quantity}</td>
-                                <td>{item.quantity*item.price} kr</td>
+                                <td><ButtonToolbar>
+                                    <MDBBtn bsStyle="primary" color="info" onClick={() => { this.handleDecrease(81, item.id) } }>-</MDBBtn>
+                                    <ListGroupItem>{item.quantity}</ListGroupItem>
+                                    <MDBBtn bsStyle="primary" color="info" onClick={() => { this.handleIncrease(81, item.id) } }>+</MDBBtn>
+                                </ButtonToolbar>
+
+                                    </td>
+                                <td><b>{item.quantity*item.price} kr</b></td>
                                 <td><MDBBtn color={"danger"} variant="primary" onClick={() => { this.handleDelete(81, item.id) } } >Remove</MDBBtn></td>
                             </tr>
                         ))}
                         </tbody>
                     </table>
                 </div>
+                </Container>
             </div>
         );
     }
