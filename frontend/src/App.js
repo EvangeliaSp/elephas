df0c3884396bc298416334eb8d518b4c85e044d4
evangelia <evangelia@lana-labs.com>
2019-03-04 14:47:26 +0100

    Made it as master's App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
+import logo from './logo2.png';
 import Footer from './components/Footer'
 import Header from './components/Header';
 import './App.css';
+import product1 from './product2.jpg'
 
 
 class App extends Component {
@@ -16,25 +18,26 @@ class App extends Component {
     }
 }
 
-class Content extends React.Component {
+class Content extends Component {
     render() {
         return (
             <div>
-                <div className="fb">
-                    <iframe
-                        src="http://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Felephasjewelry&width=600&colorscheme=light&show_faces=true&border_color&stream=true&header=true&height=435"
-                        scrolling="yes"
-                        style={{
-                            border: "none",
-                            overflow: "hidden",
-                            width: "500px",
-                            height: "400px",
-                            background: "white",
-                        }}
-
-                        allowTransparency="true"></iframe>
-
+                <div className="elephants">
+                    <img src={product1} alt="Elephas"/>
                 </div>
+                {/*<div className="fb">*/}
+                    {/*<iframe*/}
+                        {/*src="http://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Felephasjewelry&width=600&colorscheme=light&show_faces=true&border_color&stream=true&header=true&height=435"*/}
+                        {/*scrolling="yes"*/}
+                        {/*style={{*/}
+                            {/*border: "none",*/}
+                            {/*overflow: "hidden",*/}
+                            {/*width: "500px",*/}
+                            {/*height: "400px",*/}
+                            {/*background: "white",*/}
+                        {/*}}*/}
+                        {/*allowTransparency="true"></iframe>*/}
+                {/*</div>*/}
             </div>
         );
     }
