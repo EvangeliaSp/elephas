1c9c4a804cec527f36c801fe680b84fc94bf158a
knikamo <karolina@nikamo.se>
2019-03-06 20:53:48 +0100

    index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -16,6 +16,7 @@ import ProductDetail from "./product/ProductDetail";
 import About from "./components/About";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import ShowCart from "./order/ShowCart";
 
 
 ReactDOM.render(
@@ -25,10 +26,12 @@ ReactDOM.render(
             <Route exact path='/' component={App} />
             <Route path="/user/login" exact component={Login} />
             <Route path='/user/all' component={UserList} />
-            <Route path='/product/findBy' component={ProductGrid} />
+            <Route path='/product' component={ProductGrid} />
+            <Route path='/product/findBy' component={ProductGrid} /> 
             <Route path='/product/findById/:id' component={ProductDetail} />
             <Route path='/user/profile' component={Profile}/>
             <Route path='/user/register' component={Register}/>
+            <Route path='/order/cart' component={ShowCart}/>
             <Route path='/about' component={About}/>
             <Footer/>
 
