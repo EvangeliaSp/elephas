c049a73bf4abf009194a174c9492b3497197fcb7
nicolets <Nikoletta.Tsongari.9603@student.uu.se>
2019-03-07 20:06:45 +0100

    tests not working

--- frontend/src/product/FilterProduct.js
+++ frontend/src/product/FilterProduct.js
@@ -1,115 +1,115 @@
-import {
-    SearchBox,
-    Hits,
-    HitsStats,
-    RefinementListFilter,
-    Pagination,
-    ResetFilters,
-    MenuFilter,
-    SelectedFilters,
-    HierarchicalMenuFilter,
-    NumericRefinementListFilter,
-    SortingSelector,
-    SearchkitComponent,
-    SearchkitProvider,
-    SearchkitManager,
-    NoHits,
-    RangeFilter,
-    InitialLoader,
-    ViewSwitcherToggle,
-    ViewSwitcherHits,
-    Layout, LayoutBody, LayoutResults,
-    SideBar, TopBar,
-    ActionBar, ActionBarRow
-} from "searchkit";
-import * as React from "react";
-// import 'scss/modules.container.scss';
-import '/home/mikaela/elephas/frontend/src/product/filter.scss';
-import * as _ from "lodash";
-//const BEMBlock = require("bem-cn")
-declare var require: any
-
-// import "searchkit/theming/theme.scss";
-
-// import {MovieHitsGridItem, MovieHitsListItem} from "./ResultComponents"
-export class App extends React.Component<any, any> {
-
-    searchkit:SearchkitManager
-
-    constructor() {
-        super()
-        const host = "/product/findBy"
-        this.searchkit = new SearchkitManager(host)
-        this.searchkit.translateFunction = (key)=> {
-            return {"pagination.next":"Next Page", "pagination.previous":"Previous Page"}[key]
-        }
-    }
-
-
-    render(){
-
-        return (
-            <SearchkitProvider searchkit={this.searchkit}>
-                <Layout size="l">
-                    <TopBar>
-                        <div className="my-logo">Elephas</div>
-                        <SearchBox
-                            translations={{"searchbox.placeholder":"search products"}}
-                            queryOptions={{"minimum_should_match":"70%"}}
-                            autofocus={true}
-                            searchOnChange={true}
-                            queryFields={["actors^1","type^2","languages","title^5", "genres^2", "plot"]}/>
-                    </TopBar>
-
-                    <LayoutBody>
-
-                        <SideBar>
-                            <RangeFilter min={0} max={100} field="metaScore" id="metascore" title="Metascore" showHistogram={true}/>
-                            <RangeFilter min={0} max={10} field="imdbRating" id="imdbRating" title="IMDB Rating" showHistogram={true}/>
-                            <RefinementListFilter id="products" title="Products" field="products.raw" operator="OR" size={10}/>
-                        </SideBar>
-
-                        <LayoutResults>
-
-                            <ActionBar>
-
-                                <ActionBarRow>
-                                    <HitsStats translations={{
-                                        "hitstats.results_found":"{hitCount} results found"
-                                    }}/>
-                                    <ViewSwitcherToggle/>
-                                    {/*<SortingSelector options={[*/}
-                                        {/*{label:"Relevance", field:"_score", order:"desc",defaultOption:true},*/}
-                                        {/*{label:"Latest Releases", field:"released", order:"desc"},*/}
-                                        {/*{label:"Earliest Releases", field:"released", order:"asc"}*/}
-                                    {/*]}/>*/}
-                                </ActionBarRow>
-                                <ActionBarRow>
-                                    <SelectedFilters/>
-                                    <ResetFilters/>
-                                </ActionBarRow>
-
-                            </ActionBar>
-
-                            {/*<ViewSwitcherHits*/}
-                                {/*hitsPerPage={12} highlightFields={["title","plot"]}*/}
-                                {/*sourceFilter={["plot", "title", "poster", "imdbId", "imdbRating", "year"]}*/}
-                                {/*hitComponents = {[*/}
-                                    {/*{key:"grid", title:"Grid", itemComponent:MovieHitsGridItem, defaultOption:true},*/}
-                                    {/*{key:"list", title:"List", itemComponent:MovieHitsListItem}*/}
-                                {/*]}*/}
-                                {/*scrollTo="body"*/}
-                            {/*/>*/}
-
-                            <NoHits suggestionsField={"title"}/>
-                            <InitialLoader/>
-                            <Pagination showNumbers={true}/>
-                        </LayoutResults>
-                    </LayoutBody>
-                </Layout>
-            </SearchkitProvider>
-        )}
-
-}
-
-export default App
+// import {
+//     SearchBox,
+//     Hits,
+//     HitsStats,
+//     RefinementListFilter,
+//     Pagination,
+//     ResetFilters,
+//     MenuFilter,
+//     SelectedFilters,
+//     HierarchicalMenuFilter,
+//     NumericRefinementListFilter,
+//     SortingSelector,
+//     SearchkitComponent,
+//     SearchkitProvider,
+//     SearchkitManager,
+//     NoHits,
+//     RangeFilter,
+//     InitialLoader,
+//     ViewSwitcherToggle,
+//     ViewSwitcherHits,
+//     Layout, LayoutBody, LayoutResults,
+//     SideBar, TopBar,
+//     ActionBar, ActionBarRow
+// } from "searchkit";
+// import * as React from "react";
+// // import 'scss/modules.container.scss';
+// import '/home/mikaela/elephas/frontend/src/product/filter.scss';
+// import * as _ from "lodash";
+// //const BEMBlock = require("bem-cn")
+// declare var require: any
+//
+// // import "searchkit/theming/theme.scss";
+//
+// // import {MovieHitsGridItem, MovieHitsListItem} from "./ResultComponents"
+// export class App extends React.Component<any, any> {
+//
+//     searchkit:SearchkitManager
+//
+//     constructor() {
+//         super()
+//         const host = "/product/findBy"
+//         this.searchkit = new SearchkitManager(host)
+//         this.searchkit.translateFunction = (key)=> {
+//             return {"pagination.next":"Next Page", "pagination.previous":"Previous Page"}[key]
+//         }
+//     }
+//
+//
+//     render(){
+//
+//         return (
+//             <SearchkitProvider searchkit={this.searchkit}>
+//                 <Layout size="l">
+//                     <TopBar>
+//                         <div className="my-logo">Elephas</div>
+//                         <SearchBox
+//                             translations={{"searchbox.placeholder":"search products"}}
+//                             queryOptions={{"minimum_should_match":"70%"}}
+//                             autofocus={true}
+//                             searchOnChange={true}
+//                             queryFields={["actors^1","type^2","languages","title^5", "genres^2", "plot"]}/>
+//                     </TopBar>
+//
+//                     <LayoutBody>
+//
+//                         <SideBar>
+//                             <RangeFilter min={0} max={100} field="metaScore" id="metascore" title="Metascore" showHistogram={true}/>
+//                             <RangeFilter min={0} max={10} field="imdbRating" id="imdbRating" title="IMDB Rating" showHistogram={true}/>
+//                             <RefinementListFilter id="products" title="Products" field="products.raw" operator="OR" size={10}/>
+//                         </SideBar>
+//
+//                         <LayoutResults>
+//
+//                             <ActionBar>
+//
+//                                 <ActionBarRow>
+//                                     <HitsStats translations={{
+//                                         "hitstats.results_found":"{hitCount} results found"
+//                                     }}/>
+//                                     <ViewSwitcherToggle/>
+//                                     {/*<SortingSelector options={[*/}
+//                                         {/*{label:"Relevance", field:"_score", order:"desc",defaultOption:true},*/}
+//                                         {/*{label:"Latest Releases", field:"released", order:"desc"},*/}
+//                                         {/*{label:"Earliest Releases", field:"released", order:"asc"}*/}
+//                                     {/*]}/>*/}
+//                                 </ActionBarRow>
+//                                 <ActionBarRow>
+//                                     <SelectedFilters/>
+//                                     <ResetFilters/>
+//                                 </ActionBarRow>
+//
+//                             </ActionBar>
+//
+//                             {/*<ViewSwitcherHits*/}
+//                                 {/*hitsPerPage={12} highlightFields={["title","plot"]}*/}
+//                                 {/*sourceFilter={["plot", "title", "poster", "imdbId", "imdbRating", "year"]}*/}
+//                                 {/*hitComponents = {[*/}
+//                                     {/*{key:"grid", title:"Grid", itemComponent:MovieHitsGridItem, defaultOption:true},*/}
+//                                     {/*{key:"list", title:"List", itemComponent:MovieHitsListItem}*/}
+//                                 {/*]}*/}
+//                                 {/*scrollTo="body"*/}
+//                             {/*/>*/}
+//
+//                             <NoHits suggestionsField={"title"}/>
+//                             <InitialLoader/>
+//                             <Pagination showNumbers={true}/>
+//                         </LayoutResults>
+//                     </LayoutBody>
+//                 </Layout>
+//             </SearchkitProvider>
+//         )}
+//
+// }
+//
+// export default App
--- frontend/src/product/ProductGrid.js
+++ frontend/src/product/ProductGrid.js
@@ -6,6 +6,7 @@ import Button from 'react-bootstrap/Button';
 import elephas2 from './../elephas2.jpg';
 import Header from './../components/Header';
 import Footer from './../components/Footer';
+import {Checkbox, CheckboxGroup} from 'react-checkbox-group';
 
 class ProductGrid extends Component {
 
@@ -16,8 +17,11 @@ class ProductGrid extends Component {
 
         this.state = {
             products: [],
-            isLoading: true
+            isLoading: true,
+            colors: [1, 2, 3]
         }
+
+
     }
 
     componentDidMount() {
@@ -30,35 +34,44 @@ class ProductGrid extends Component {
         fetch(`/product/findBy${this.props.location.search}`)
             .then(response => response.json())
             .then(data => this.setState({products: data, isLoading: false}))
+
     };
 
 
     ProductCard(product) {
         //TODO: Product name instead of card title
-        return(
+        return (
             <div key={product.idProduct} className="col-sm-6 col-lg-4">
-            <Card style={{ marginBottom: '2rem'}}>
-                <Card.Img variant="top" src={elephas2} />
-                <Card.Body>
-                    <Card.Title>{product.name}</Card.Title> 
-                    <Card.Text>
-                        {product.description}<br/>
-                        
-                    </Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                {product.price} kr
-                <Button variant="primary" href="#addToBasket" style={{float:'right'}}>Add to basket</Button>
-                </Card.Footer>
-            </Card>
+                <Card style={{marginBottom: '2rem'}}>
+                    <Card.Img variant="top" src={elephas2}/>
+                    <Card.Body>
+                        <Card.Title>{product.name}</Card.Title>
+                        <Card.Text>
+                            {product.description}<br/>
+
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                        {product.price} kr
+                        <Button variant="primary" href="#addToBasket" style={{float: 'right'}}>Add to basket</Button>
+                    </Card.Footer>
+                </Card>
             </div>
         );
     }
 
- 
+    handleChange = (e, value) => {
+        console.log(value);
+        console.log(e.target.checked);
+        if (value==1 && e.target.checked==true)
+            var mpla=this.props.location.search
+            this.props.location.search=this.props.location.search+"?type=2";
+            console.log(this.props.location.search)
+        return (e,value);
+    }
 
     render() {
-        const  {products, isLoading} = this.state
+        const {products, isLoading} = this.state
 
         if (isLoading)
             return <p>Loading...</p>
@@ -66,14 +79,23 @@ class ProductGrid extends Component {
         return (
             <div>
                 <Header/>
+
+                <form>
+                    <input  type="checkbox" name="color" value="1"
+                            onClickCapture={(e) => this.handleChange(e, 1)}/>White
+                    <input onChange={this.handleInputChange} type="checkbox" name="color" value="2"
+                           onChange={(e, value) => this.handleChange(e, 2)}/>Black
+                    <input onChange={this.handleInputChange} type="checkbox" name="color" value="3"
+                           onChange={(e, value) => this.handleChange(e, 3)}/>Grey
+                </form>
                 <Container>
-                    <Row style={{marginBottom:'7rem'}}>
-                {
-                    products.map((product) => {
-                        return this.ProductCard(product);
-                    
-                    })
-                }
+                    <Row style={{marginBottom: '7rem'}}>
+                        {
+                            products.map((product) => {
+                                return this.ProductCard(product);
+
+                            })
+                        }
                     </Row>
                 </Container>
                 <Footer/>
