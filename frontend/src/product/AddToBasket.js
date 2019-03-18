c6b8539e118ac61b572e6e2aaef89697be998458
evangelia <evangelia@lana-labs.com>
2019-03-18 21:10:16 +0100

    Removed commented code

--- frontend/src/product/AddToBasket.js
+++ frontend/src/product/AddToBasket.js
@@ -4,7 +4,6 @@ export const myBasket = (idItem) => {
     const idUser = localStorage.getItem('idUser');
     
     if (idUser === 'undefined' || idUser == null) {
-        // return <Redirect to={ ShowCart }/>
         window.location.href=`/order/cart`;
     }
 
