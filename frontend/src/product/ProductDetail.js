3a4c1ba494e4a4078392703941ffc7ceac4e967a
evangelia <evangelia@lana-labs.com>
2019-03-21 03:21:48 +0100

    Show the discount and the final price of the product

--- frontend/src/product/ProductDetail.js
+++ frontend/src/product/ProductDetail.js
@@ -4,6 +4,8 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
 import {getColor, getMaterial, getType} from '../Translations';
+import "./Product.css"
+import {myBasket} from "./AddToBasket";
 
 class ProductDetail extends Component {
 
@@ -32,12 +34,32 @@ class ProductDetail extends Component {
 
 
     ProductDetailCard(product) {
+        if( product.discount != 0) {
+            return(
+                <div key={product.idProduct} className="col-sm-12 col-lg-8 mx-auto">
+                    <Card style={{ marginBottom: '2rem'}}>
+                        <Card.Img variant="top" src={product.url} />
+                        <Card.Body>
+                                <Card.Title>{product.name}<b className="redtext">{" (Discount:" + product.discount + "%)"}</b></Card.Title>
+                            <Card.Text>
+                                {product.description}<br/>
+                                Made of: {getMaterial(product.material)}
+                            </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                            <del>{product.price} kr</del><b className="redtext">{" " + (product.price-product.discount*product.price/100) + "kr"}</b>
+                            <Button variant="primary" onClick={() => myBasket(product.idProduct)} style={{float:'right'}}>Add to basket</Button>
+                        </Card.Footer>
+                    </Card>
+                </div>
+            )
+        }
         return(
             <div key={product.idProduct} className="col-sm-12 col-lg-8 mx-auto">
             <Card style={{ marginBottom: '2rem'}}>
                 <Card.Img variant="top" src={product.url} />
                 <Card.Body>
-                    <Card.Title>{product.name}</Card.Title> 
+                    <Card.Title>{product.name}</Card.Title>
                     <Card.Text>
                         {product.description}<br/>
                         Made of: {getMaterial(product.material)}
@@ -45,7 +67,7 @@ class ProductDetail extends Component {
                 </Card.Body>
                 <Card.Footer>
                 {product.price} kr
-                <Button variant="primary" href="#addToBasket" style={{float:'right'}}>Add to basket</Button>
+                <Button variant="primary" onClick={() => myBasket(product.idProduct)} style={{float:'right'}}>Add to basket</Button>
                 </Card.Footer>
             </Card>
             </div>
@@ -60,7 +82,7 @@ class ProductDetail extends Component {
         if (isLoading)
              return <div className="loading">
             <div className="loader"></div>
-        </div>
+        </div>;
 
 
         return (
