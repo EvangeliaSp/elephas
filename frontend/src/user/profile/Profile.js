57804e389f9312bbb5538484562fd0d940d8b829
evangelia <evangelia@lana-labs.com>
2019-03-22 04:44:28 +0100

    Add all the users' info in userList

--- frontend/src/user/profile/Profile.js
+++ frontend/src/user/profile/Profile.js
@@ -878,21 +878,25 @@ class Profile extends Component {
                 <table className="table table-striped ">
                     <thead>
                     <tr>
-                        <th scope="col">#</th>
                         <th scope="col">Avatar</th>
                         <th scope="col">Firstname</th>
                         <th scope="col">Lastname</th>
                         <th scope="col">Email</th>
+                        <th scope="col">Location</th>
+                        <th scope="col">Address</th>
+                        <th scope="col">Telephone</th>
                     </tr>
                     </thead>
                     <tbody>
                     {users.map(user => (
                         <tr key={user.id}>
-                            <th scope="row">{user.idUser}</th>
                             <td><Avatar name={user.firstname+" "+user.lastname} round size="35" /></td>
                             <td>{user.firstname}</td>
                             <td>{user.lastname}</td>
                             <td>{user.email}</td>
+                            <td>{user.city+", "+user.country}</td>
+                            <td>{user.streetName+", "+user.streetNumber+", "+user.zipCode}</td>
+                            <td>{user.telephone}</td>
                         </tr>
                     ))}
                     </tbody>
