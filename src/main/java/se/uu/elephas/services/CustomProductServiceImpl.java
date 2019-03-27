d630de8cfe26be5bb4fdcb954f2b00528f5b1cbc
evangelia <evangelia@lana-labs.com>
2019-03-27 21:05:27 +0100

    Optimized imports

--- src/main/java/se/uu/elephas/services/CustomProductServiceImpl.java
+++ src/main/java/se/uu/elephas/services/CustomProductServiceImpl.java
@@ -3,10 +3,7 @@ package se.uu.elephas.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import se.uu.elephas.model.CustomProduct;
-import se.uu.elephas.model.Product;
 import se.uu.elephas.repository.CustomProductRepository;
-import se.uu.elephas.repository.MaterialRepository;
-import se.uu.elephas.repository.ProductRepository;
 
 @Service
 public class CustomProductServiceImpl implements CustomProductService {
